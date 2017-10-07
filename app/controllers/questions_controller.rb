dd01cbdd44912ae0793b4ecc82664b9d05e12ff2
georgiybykov <zhora.bykov@gmail.com>
2017-10-08 02:25:29 +0300

    Add responder gem. Need to fix one mistake answers_path

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -2,64 +2,35 @@ class QuestionsController < ApplicationController
   before_action :authenticate_user!
   before_action :set_question, only: [:show, :edit, :update, :destroy]
 
-  # GET /questions
-  # GET /questions.json
+  respond_to :json, :html
+
   def index
-    @questions = Question.all
+    respond_with(@questions = Question.all)
   end
 
-  # GET /questions/1
-  # GET /questions/1.json
   def show
   end
 
-  # GET /questions/new
   def new
-    @question = Question.new
+    respond_with(@question = Question.new)
   end
 
-  # GET /questions/1/edit
   def edit
   end
 
-  # POST /questions
-  # POST /questions.json
   def create
     @question = Question.new(question_params)
-
-    respond_to do |format|
-      if @question.save
-        format.html { redirect_to @question, notice: 'Question was successfully created.' }
-        format.json { render :show, status: :created, location: @question }
-      else
-        format.html { render :new }
-        format.json { render json: @question.errors, status: :unprocessable_entity }
-      end
-    end
+    respond_with(@question)
   end
 
-  # PATCH/PUT /questions/1
-  # PATCH/PUT /questions/1.json
   def update
-    respond_to do |format|
-      if @question.update(question_params)
-        format.html { redirect_to @question, notice: 'Question was successfully updated.' }
-        format.json { render :show, status: :ok, location: @question }
-      else
-        format.html { render :edit }
-        format.json { render json: @question.errors, status: :unprocessable_entity }
-      end
-    end
+    @question.update(question_params)
+    respond_with(@question)
   end
 
-  # DELETE /questions/1
-  # DELETE /questions/1.json
   def destroy
     @question.destroy
-    respond_to do |format|
-      format.html { redirect_to questions_url, notice: 'Question was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    respond_with(@question)
   end
 
   private
