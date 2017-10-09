bd89fd8c8ec7fd8b2ad9d1b5a1b90c68f6ee449e
georgiybykov <zhora.bykov@gmail.com>
2017-10-09 23:46:08 +0300

    #4 Added responder gem; Added answers; Modified show action for questions

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -2,64 +2,35 @@ class AnswersController < ApplicationController
   before_action :set_question
   before_action :set_answer, only: [:show, :edit, :update, :destroy]
 
-  # GET /answers
-  # GET /answers.json
+  respond_to :json, :html
+
   def index
-    @answers = Answer.all
+    respond_with(@answers = Answer.all)
   end
 
-  # GET /answers/1
-  # GET /answers/1.json
   def show
   end
 
-  # GET /answers/new
   def new
-    @answer = Answer.new
+    respond_with(@answer = Answer.new)
   end
 
-  # GET /answers/1/edit
   def edit
   end
 
-  # POST /answers
-  # POST /answers.json
   def create
     @answer = Answer.create(answer_params)
-
-    respond_to do |format|
-      if @answer.save
-        format.html { redirect_to @answer, notice: 'Answer was successfully created.' }
-        format.json { render :show, status: :created, location: @answer }
-      else
-        format.html { render :new }
-        format.json { render json: @answer.errors, status: :unprocessable_entity }
-      end
-    end
+    respond_with(@answer)
   end
 
-  # PATCH/PUT /answers/1
-  # PATCH/PUT /answers/1.json
   def update
-    respond_to do |format|
-      if @answer.update(answer_params)
-        format.html { redirect_to @answer, notice: 'Answer was successfully updated.' }
-        format.json { render :show, status: :ok, location: @answer }
-      else
-        format.html { render :edit }
-        format.json { render json: @answer.errors, status: :unprocessable_entity }
-      end
-    end
+    @answer.update(answer_params)
+    respond_with(@answer)
   end
 
-  # DELETE /answers/1
-  # DELETE /answers/1.json
   def destroy
     @answer.destroy
-    respond_to do |format|
-      format.html { redirect_to answers_url, notice: 'Answer was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    respond_with(@answer)
   end
 
   private
